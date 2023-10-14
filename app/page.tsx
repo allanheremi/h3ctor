821abaa4783411d25d6560921980b82e4f14fae7
Allan Heremi <allanheremidev@gmail.com>
2023-10-14 17:08:32 +0200

    Chore: trying to fix mobile bug

--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,7 @@ export default function Home() {
     <>
       <div className="wrapper">
         <div className="box">
-          {Array.from({ length: 6 }).map((_, index) => (
-            <div key={index}>
-              <img
-                src={`/koda${index}.png`}
-                alt=""
-                className="rounded-full object-cover opacity-20"
-              />
-            </div>
-          ))}
+  
         </div>
       </div>
       <Hero />
