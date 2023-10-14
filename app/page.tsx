971c7df0370f0e314b010d2fbecad76459cea3ed
Allan Heremi <allanheremidev@gmail.com>
2023-10-14 19:53:39 +0200

    Fix: animation opacity CSS overwrite

--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,57 @@ export default function Home() {
   return (
     <>
       <div className="wrapper">
-        <div className='box'>
-          <img src="/koda1.jpeg" alt="" className='opacity-10 max-w-[24px] max-h-[24px] rounded-full ' />
-          <img src="/koda2.jpeg" alt="" className='opacity-10 max-w-[24px] max-h-[24px] rounded-full ' />
-          <img src="/koda3.jpeg" alt="" className='opacity-10 max-w-[24px] max-h-[24px] rounded-full ' />
-          <img src="/koda4.jpeg" alt="" className='opacity-10 max-w-[24px] max-h-[24px] rounded-full ' />
-          <img src="/koda5.jpeg" alt="" className='opacity-10 max-w-[24px] max-h-[24px] rounded-full ' />
-          <img src="/koda6.jpeg" alt="" className='opacity-10 max-w-[24px] max-h-[24px] rounded-full ' />
-          <img src="/h3ctorbg.jpeg" alt="" className='opacity-10 max-w-[24px] max-h-[24px] rounded-full ' />
-          <img src="/koda2.jpeg" alt="" className='opacity-10 max-w-[24px] max-h-[24px] rounded-full ' />
-          <img src="/h3ctorbg.jpeg" alt="" className='opacity-10 max-w-[24px] max-h-[24px] rounded-full ' />
-          <img src="/koda4.jpeg" alt="" className='opacity-10 max-w-[24px] max-h-[24px] rounded-full ' />
+        <div className="box">
+          <img
+            src="/koda1.jpeg"
+            alt=""
+            className=" max-w-[24px] max-h-[24px] rounded-full "
+          />
+          <img
+            src="/koda2.jpeg"
+            alt=""
+            className=" max-w-[24px] max-h-[24px] rounded-full "
+          />
+          <img
+            src="/koda3.jpeg"
+            alt=""
+            className=" max-w-[24px] max-h-[24px] rounded-full "
+          />
+          <img
+            src="/koda4.jpeg"
+            alt=""
+            className=" max-w-[24px] max-h-[24px] rounded-full "
+          />
+          <img
+            src="/koda5.jpeg"
+            alt=""
+            className=" max-w-[24px] max-h-[24px] rounded-full "
+          />
+          <img
+            src="/koda6.jpeg"
+            alt=""
+            className=" max-w-[24px] max-h-[24px] rounded-full "
+          />
+          <img
+            src="/h3ctorbg.jpeg"
+            alt=""
+            className=" max-w-[24px] max-h-[24px] rounded-full "
+          />
+          <img
+            src="/koda2.jpeg"
+            alt=""
+            className=" max-w-[24px] max-h-[24px] rounded-full "
+          />
+          <img
+            src="/h3ctorbg.jpeg"
+            alt=""
+            className=" max-w-[24px] max-h-[24px] rounded-full "
+          />
+          <img
+            src="/koda4.jpeg"
+            alt=""
+            className=" max-w-[24px] max-h-[24px] rounded-full "
+          />
         </div>
         <Hero />
         <Contact />
